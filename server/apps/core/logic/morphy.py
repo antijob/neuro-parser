e80c5423cbd9bf8c52a516e1eff8c913f7179892
ubiq <explesy@gmail.com>
2023-06-30 20:06:29 +0400

    FIX for nltk

--- server/apps/core/logic/morphy.py
+++ server/apps/core/logic/morphy.py
@@ -7,6 +7,8 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 USEFULL_GRAMMEMES = ['NOUN', 'VERB', 'ADJF', 'ADJS', 'INFN',
