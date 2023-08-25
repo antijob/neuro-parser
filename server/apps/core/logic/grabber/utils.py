2757eae91fda494b19baa4e6f3caf0cd97a79a49
Aleksandr Dumarevskii <sahdoum@gmail.com>
2023-08-25 20:50:17 +0200

    FIX convert_date_format

--- server/apps/core/logic/grabber/utils.py
+++ server/apps/core/logic/grabber/utils.py
@@ -5,10 +5,13 @@ def convert_date_format(date_string: str) -> str:
     '''
     Parse data string and return in format %Y-%m-%d
     '''
-    # date_obj = dateparser.parse(date_string, languages=['en', 'ru'], settings={'TO_TIMEZONE': 'UTC'})
+    if not date_string:
+        return None
+
     date_obj = dateparser.parse(date_string, languages=['en', 'ru'])
     if date_obj:
         utc_date = date_obj.strftime('%Y-%m-%d')
         return utc_date
     else:
-        return "Invalid date format!"
+        print("Invalid date format: ", date_string)
+        return None
