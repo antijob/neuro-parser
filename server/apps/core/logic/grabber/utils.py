2989554703c7dcc9d0715c003d546fdedfe461b5
ars <you@example.com>
2023-08-27 13:55:29 +0400

    FIX none in undefined date to curent date

--- server/apps/core/logic/grabber/utils.py
+++ server/apps/core/logic/grabber/utils.py
@@ -1,12 +1,15 @@
 # Some functions that used across different modules
 import dateparser
+from datetime import datetime
 
 def convert_date_format(date_string: str) -> str:
     '''
     Parse data string and return in format %Y-%m-%d
     '''
+    current_date = datetime.now().strftime('%Y-%m-%d')
+
     if not date_string:
-        return None
+        return current_date
 
     date_obj = dateparser.parse(date_string, languages=['en', 'ru'])
     if date_obj:
@@ -14,4 +17,4 @@ def convert_date_format(date_string: str) -> str:
         return utc_date
     else:
         print("Invalid date format: ", date_string)
-        return None
+        return current_date
