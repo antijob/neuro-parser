6ad217dc583d0cbf4d7455a0d9ef51234632fc6e
Aleksandr Dumarevskii <sahdoum@gmail.com>
2023-09-01 16:51:34 +0200

    FIX Reorder migrations, remove annotated_title

--- server/apps/core/migrations/0068_change_incidenttype.py
+++ server/apps/core/migrations/0068_change_incidenttype.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0065_delete_dataleak'),
+        ('core', '0067_delete_stage'),
     ]
 
     operations = [
