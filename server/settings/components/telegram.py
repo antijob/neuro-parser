00e62935d32bf133bff27ad564a6ef117e26a760
ubiq <explesy@gmail.com>
2023-09-07 12:31:11 +0400

    ADD settings fos tg bot

--- server/settings/components/telegram.py
+++ server/settings/components/telegram.py
@@ -4,7 +4,8 @@
 Telegram bot configuration module
 """
 
-from decouple import config  as c
+from decouple import config as c
 c.path='../../../.env'
 
 TELEGRAM_BOT_TOKEN = c('TELEGRAM_BOT_TOKEN')
+TELEGRAM_BOT_NAME = c('TELEGRAM_BOT_NAME')
