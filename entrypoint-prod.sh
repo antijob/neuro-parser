c0e8e118ca5aa3b30c6eb659302322de6c51d017
ars <you@example.com>
2023-09-17 19:27:11 +0400

    FIX entrypoint prod

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -4,7 +4,7 @@
 while !</dev/tcp/db/5432; do sleep 1; done
 
 # Start the Django application
-python while !</dev/tcp/db/5432; do sleep 1;done; gunicorn --bind 0.0.0.0:8000 server.wsgi &
+python gunicorn --bind 0.0.0.0:8000 server.wsgi &
 
 # Start the Celery worker
 celery -A server worker --loglevel=info
