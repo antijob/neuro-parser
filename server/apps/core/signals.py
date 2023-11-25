f2b49214c184d366b08a637c3322c4d0a29d2566
ars <you@example.com>
2023-11-25 15:58:16 +0400

    feat: add exeption handlerto signal incident created

--- server/apps/core/signals.py
+++ server/apps/core/signals.py
@@ -1,6 +1,5 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from server.apps.core.incident_types import IncidentType
 from .models import MediaIncident
 from server.apps.bot.models import Channel, TypeStatus
 from server.apps.bot.logic.send import send_message
@@ -13,6 +12,7 @@ Description: {desc}
 URLs: {url}
 """
 
+
 @receiver(post_save, sender=MediaIncident)
 def mediaincident_post_save(sender, instance, created, **kwargs):
     """
@@ -22,13 +22,17 @@ def mediaincident_post_save(sender, instance, created, **kwargs):
     if created:
         all_channels = Channel.objects.all()
         msg = inc_template.format(
-                                    cat=instance.incident_type.description,
-                                    title=instance.title,
-                                    desc=instance.description,
-                                    url=' '.join(instance.urls),
-                                    )
+            cat=instance.incident_type.description,
+            title=instance.title,
+            desc=instance.description,
+            url=' '.join(instance.urls),
+        )
         for chn in all_channels:
-            type_status = TypeStatus.objects.get(channel=chn, incident_type=instance.incident_type)
+            try:
+                type_status = TypeStatus.objects.get(
+                    channel=chn, incident_type=instance.incident_type)
+            except Exception as e:
+                print('An error occurred: ', e)
+                print('instanct: ', instance)
             if type_status.status:
                 send_message(chn.channel_id, msg)
-
