751642388c5da746c0ba8f32da79fafd0bafc9c1
ubiq <explesy@gmail.com>
2024-05-30 10:18:10 +0200

    add: region and country separate models

--- server/apps/core/signals.py
+++ server/apps/core/signals.py
@@ -1,8 +1,25 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
-from .models import MediaIncident
-from server.apps.bot.models import Channel, TypeStatus
+
 from server.apps.bot.logic.send import send_message
+from server.apps.bot.models import Channel, TypeStatus
+from server.apps.core.data.regions import COUNTRIES, REGIONS
+
+from .models import Country, MediaIncident, Region
+
+
+@receiver(post_migrate)
+def create_default_data(sender, **kwargs):
+    """
+    Function that will be executed on each migration
+    """
+    for country in COUNTRIES:
+        Country.objects.get_or_create(name=country[0])
+    for region in REGIONS:
+        Region.objects.get_or_create(
+            name=region[0], country=Country.objects.get(name="RUS")
+        )
+
 
 inc_template = """
 New incident created
@@ -25,14 +42,15 @@ def mediaincident_post_save(sender, instance, created, **kwargs):
             cat=instance.incident_type.description,
             title=instance.title,
             desc=instance.description,
-            url=' '.join(instance.urls),
+            url=" ".join(instance.urls),
         )
         for chn in all_channels:
             try:
                 type_status = TypeStatus.objects.get(
-                    channel=chn, incident_type=instance.incident_type)
+                    channel=chn, incident_type=instance.incident_type
+                )
                 if type_status.status:
                     send_message(chn.channel_id, msg)
             except Exception as e:
-                print('An error occurred: ', e)
-                print('instance: ', instance.id)
+                print("An error occurred: ", e)
+                print("instance: ", instance.id)
