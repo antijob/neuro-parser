bb8a6a9185680ef293fecce725f659962e77a77a
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-06-08 18:36:26 +0400

    move fetcher to core

--- server/core/fetcher.py
+++ server/core/fetcher.py
@@ -1,25 +1,35 @@
 # fetcher.py
-import requests
 import asyncio
 import aiohttp
 import time
-from icecream import ic
 
 from asgiref.sync import sync_to_async
 from .user_agent import session_random_headers
 
 
+async def article_postprocess(article, content):
+    if content is None:
+        return 0
+    return await sync_to_async(article.get_html_and_postprocess)(content)
+
+
+async def source_postprocess(source, content):
+    if content is None:
+        return 0
+    return 1
+
+
 class BadCodeException(Exception):
-    def __init__(self, code):            
-        super().__init__('Bad code')
-            
+    def __init__(self, code):
+        super().__init__("Bad code")
+
         self.code = code
 
 
 async def fetch_url(session, url):
     params = {}
-    if url.startswith('https://t.me/'):
-        params = {'embed': '1'}
+    if url.startswith("https://t.me/"):
+        params = {"embed": "1"}
     async with session.get(url, params=params) as response:
         if response.status == 200:
             return await response.text()
@@ -30,7 +40,7 @@ async def fetch_url(session, url):
 
 class Fetcher(object):
     def __new__(cls):
-        if not hasattr(cls, 'instance'):
+        if not hasattr(cls, "instance"):
             cls.instance = super(Fetcher, cls).__new__(cls)
         return cls.instance
 
@@ -85,27 +95,28 @@ class Fetcher(object):
         def __str__(self):
             codes_str = "Bad codes info: "
             for code, count in self.codes.items():
-                codes_str += f"\"{code}\": {count}, "
-            return (f"Coroutine finished. Source: {self.source}: {self.len} articles by {self.elapsed_time:.2f}s.\n"
-                    f"Fetch time: {self.fetch_time:.2f}s, Parse time:{self.postprocess_time:.2f}s\n"
-                    f"Succeded: {self.success}, Fetched: {self.fetches}, Bad Codes: {self.bad_codes}, Exceptions: {self.exceptions}\n"
-                    f"{codes_str}"
-                    )
-    
-    async def coroutine(self, source, articles):
-        rps = 1 #source.rps
-        if '.ok.ru' in source.url or 't.me' in source.url:
-            rps = .1
+                codes_str += f'"{code}": {count}, '
+            return (
+                f"Coroutine finished. Source: {self.source}: {self.len} articles by {self.elapsed_time:.2f}s.\n"
+                f"Fetch time: {self.fetch_time:.2f}s, Parse time:{self.postprocess_time:.2f}s\n"
+                f"Succeded: {self.success}, Fetched: {self.fetches}, Bad Codes: {self.bad_codes}, Exceptions: {self.exceptions}\n"
+                f"{codes_str}"
+            )
+
+    async def coroutine(self, source, articles, postprocess_function):
+        rps = 1  # source.rps
+        if ".ok.ru" in source.url or "t.me" in source.url:
+            rps = 0.1
 
         print(f"Start coroutine. Source {source.url}: {len(articles)} articles")
         statistics = Fetcher.CoroutineStatistics(source.url, len(articles))
 
         async with aiohttp.ClientSession(
-            trust_env = True, 
-            connector=aiohttp.TCPConnector(ssl=False), 
-            headers=session_random_headers()
+            trust_env=True,
+            connector=aiohttp.TCPConnector(ssl=False),
+            headers=session_random_headers(),
         ) as session:
-            delay = 1 / rps 
+            delay = 1 / rps
             statistics.start()
 
             for url, article in articles.items():
@@ -114,9 +125,8 @@ class Fetcher(object):
                     content = await fetch_url(session, url)
                     statistics.fetched()
 
-                    if content is not None:
-                        postprocess_time = await sync_to_async(article.get_html_and_postprocess)(content)
-                        statistics.postprocess(postprocess_time)
+                    postprocess_time = postprocess_function(article, content)
+                    statistics.postprocess(postprocess_time)
 
                     await asyncio.sleep(delay)
                 except BadCodeException as e:
@@ -129,8 +139,8 @@ class Fetcher(object):
             print(statistics)
 
     def add_coroutine(self, source, articles):
-        articles = {a.url:a for a in articles}
-        coro = self.coroutine(source, articles)
+        articles = {a.url: a for a in articles}
+        coro = self.coroutine(source, articles, article_postprocess)
 
         self.coroutines.append(coro)
 
