d0a04d07deb1a7fa1315955ce262b5f9fe07bca7
ubiq <explesy@gmail.com>
2024-06-25 00:51:40 +0200

    fix: logging for bot/logic/send

--- server/apps/bot/logic/send.py
+++ server/apps/bot/logic/send.py
@@ -1,14 +1,16 @@
+import logging
+
 from telegram import Bot
+
 from server.settings.components.telegram import TELEGRAM_BOT_TOKEN
 
+logger = logging.getLogger(__name__)
+
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
 
 def send_message(user_id, message):
     try:
         bot.send_message(chat_id=user_id, text=message)
     except Exception as e:
-        print('An error occurred: ', e)
-        print('Chat ID: ', user_id)
-
-# def send_incident():
-
+        logger.error(f"Error while sending message: {e}")
