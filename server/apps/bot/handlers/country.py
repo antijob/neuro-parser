2dacaa262d135da4d6a66f74c88867327eb22fd2
ubiq <explesy@gmail.com>
2024-07-07 03:32:28 +0200

    add: back button for country keyboard

--- server/apps/bot/handlers/country.py
+++ server/apps/bot/handlers/country.py
@@ -1,13 +1,15 @@
 import logging
 
 from aiogram import Router
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery
 from asgiref.sync import sync_to_async
 from magic_filter import F
 
+from server.apps.bot.data.messages import CATEGORIES_MESSAGE
 from server.apps.bot.handlers.utils import chat_in_db
+from server.apps.bot.keyboards.category_kb import category_keyboard
 from server.apps.bot.keyboards.country_kb import CountryCF, country_keyboard
-from server.apps.bot.models import ChannelCountry, ChannelIncidentType, IncidentType
+from server.apps.bot.models import ChannelCountry
 
 logger = logging.getLogger(__name__)
 router = Router()
@@ -37,3 +39,13 @@ async def category_config_callback(
     keyboard = await country_keyboard(cit)
     await callback.message.edit_reply_markup(reply_markup=keyboard)
     await callback.answer()
+
+
+@router.callback_query(CountryCF.filter(F.action == "back"))
+async def back_to_category(callback: CallbackQuery):
+    chn = await chat_in_db(callback.message)
+    if not chn:
+        return None
+
+    keyboard = await category_keyboard(chn)
+    await callback.message.edit_text(text=CATEGORIES_MESSAGE, reply_markup=keyboard)
--- server/apps/bot/keyboards/country_kb.py
+++ server/apps/bot/keyboards/country_kb.py
@@ -3,7 +3,7 @@ from enum import Enum
 from typing import Optional
 
 from aiogram.filters.callback_data import CallbackData
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from asgiref.sync import sync_to_async
 
@@ -19,11 +19,12 @@ logger = logging.getLogger(__name__)
 class Action(str, Enum):
     update = "update"
     region = "region"
+    back = "back"
 
 
 class CountryCF(CallbackData, prefix="cnt"):
     action: Action
-    channel_country_id: int
+    channel_country_id: Optional[int]
 
 
 @sync_to_async
@@ -41,19 +42,27 @@ def country_keyboard(cit: ChannelIncidentType) -> Optional[InlineKeyboardMarkup]
     for ch_country in countries:
         btn_label = ch_country.country.get_full_country_name()
         status = CHECK if ch_country.status else CROSS
+        ch_country_id = ch_country.id
 
         builder.button(
             text=btn_label,
             callback_data=CountryCF(
-                action=Action.update, channel_country_id=ch_country.id
+                action=Action.update, channel_country_id=ch_country_id
             ),
         )
         builder.button(
             text=status,
             callback_data=CountryCF(
-                action=Action.update, channel_country_id=ch_country.id
+                action=Action.update, channel_country_id=ch_country_id
             ),
         )
-
     builder.adjust(2)
-    return builder.as_markup()
+
+    back_button = InlineKeyboardButton(
+        text="<< Назад",
+        callback_data=CountryCF(action=Action.back, channel_country_id=None).pack(),
+    )
+    keyboard = builder.as_markup()
+    keyboard.inline_keyboard.append([back_button])
+
+    return keyboard
