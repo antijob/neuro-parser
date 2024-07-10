9e192f4ab584ebd43f634b031e0be1fe29ab8200
ubiq <explesy@gmail.com>
2024-07-10 12:45:03 +0200

    del: unused bot settings
    Name and url for bot was deleted

--- server/settings/components/telegram.py
+++ server/settings/components/telegram.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-
 """
 Telegram bot configuration module
 """
 
 from decouple import config as c
-c.path='../../../.env'
 
-TELEGRAM_BOT_TOKEN = c('TELEGRAM_BOT_TOKEN')
-TELEGRAM_BOT_NAME = c('TELEGRAM_BOT_NAME')
-TELEGRAM_BOT_URL = c('TELEGRAM_BOT_URL')
+c.path = "../../../.env"
+
+TELEGRAM_BOT_TOKEN = c("TELEGRAM_BOT_TOKEN")
