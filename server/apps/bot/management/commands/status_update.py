81c32feb5f5cc478b99c5b58688446e15cee9e44
ubiq <explesy@gmail.com>
2024-07-11 11:01:29 +0200

    upd: status update command for working with new bot models

--- server/apps/bot/management/commands/status_update.py
+++ server/apps/bot/management/commands/status_update.py
@@ -1,33 +1,48 @@
-from django.core.management.base import BaseCommand
-
-from server.apps.bot.models import Channel, CountryStatus, RegionStatus, TypeStatus
-from server.apps.core.models import IncidentType, Country, Region
-
 import logging
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from server.apps.bot.models import Channel, ChannelIncidentType, ChannelCountry
+from server.apps.core.incident_types import IncidentType
+from server.apps.core.models import Country, Region
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        all_channels = Channel.objects.all()
-        for chn in all_channels:
-            for incident in IncidentType.objects.all():
-                if not TypeStatus.objects.filter(
-                    incident_type=incident, channel=chn
-                ).exists():
-                    TypeStatus.objects.create(
-                        incident_type=incident, channel=chn, status=True
-                    )
-            for country in Country.objects.all():
-                if not CountryStatus.objects.filter(
-                    country=country, channel=chn
-                ).exists():
-                    CountryStatus.objects.create(
-                        country=country, channel=chn, status=True
-                    )
-            for region in Region.objects.all():
-                if not RegionStatus.objects.filter(region=region, channel=chn).exists():
-                    RegionStatus.objects.create(region=region, channel=chn, status=True)
+    help = "Updates or creates ChannelIncidentType and ChannelCountry for all channels"
 
-        logger.info("All statuses created or already exists")
+    def handle(self, *args, **options):
+        with transaction.atomic():
+            all_channels = Channel.objects.all()
+            for channel in all_channels:
+                self.process_channel(channel)
+
+        logger.info("All statuses created or updated successfully")
+
+    def process_channel(self, channel):
+        # Process IncidentTypes
+        for incident_type in IncidentType.objects.all():
+            channel_incident_type, created = ChannelIncidentType.objects.get_or_create(
+                channel=channel, incident_type=incident_type, defaults={"status": True}
+            )
+            if created:
+                logger.info(
+                    f"Created ChannelIncidentType for channel {channel.channel_id} and incident type {incident_type}"
+                )
+
+            # Process Countries for each ChannelIncidentType
+            for country in Country.objects.all():
+                channel_country, created = ChannelCountry.objects.get_or_create(
+                    channel_incident_type=channel_incident_type,
+                    country=country,
+                    defaults={
+                        "status": True,
+                        "enabled_regions": (
+                            country.get_region_codes() if country.has_region() else []
+                        ),
+                    },
+                )
+                if created:
+                    logger.info(
+                        f"Created ChannelCountry for channel {channel.channel_id}, incident type {incident_type}, and country {country}"
+                    )
