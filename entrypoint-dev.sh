deae8157d8f4dc5f2c7869dd23c6f82410b45f57
ubiq <explesy@gmail.com>
2024-07-12 10:13:04 +0200

    fix(entry): uncomment

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -4,10 +4,10 @@
 while ! </dev/tcp/db/5432; do sleep 1; done
 
 # Apply database migrations
-# python manage.py migrate
+python manage.py migrate
 
 # Run the status update script
-# python manage.py status_update
+python manage.py status_update
 
 # Start the Django application
 python manage.py runserver 0.0.0.0:8000 &
