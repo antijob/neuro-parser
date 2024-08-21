4012a3c4ce55e869f4088c8d98ab64eac265a953
Vldln <joker5916@gmail.com>
2024-08-21 12:17:45 +0400

    fix: delete debugs lines and change model name to var

--- server/core/incident_predictor/incident_predictor.py
+++ server/core/incident_predictor/incident_predictor.py
@@ -101,7 +101,6 @@ class IncidentPredictor:
                 article.save()
 
                 is_incident = bool(re.search(r'[+]', response.data))
-                logger.debug(is_incident)
                 return is_incident
             else:
                 relevance = rate_with_model_and_tokenizer(
