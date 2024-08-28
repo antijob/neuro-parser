e782952176f3c37332d08ce3c0bc5524483b0161
Vldln <joker5916@gmail.com>
2024-08-28 21:14:14 +0400

    fix: configurage sentry

--- server/settings/components/sentry.py
+++ server/settings/components/sentry.py
@@ -9,13 +9,21 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 from decouple import config as c
 
-c.path='../../../.env'
+c.path = '../../../.env'
 SENTRY_DSN = c('SENTRY_DSN')
+
+
+def before_send(event, hint):
+    if event.get('level') == 'warning':
+        return None
+    return event
+
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
-    integrations=[
-        DjangoIntegration(),
-    ],
+    integrations=[DjangoIntegration()],
     send_default_pii=True,
+    before_send=before_send,
+    sample_rate=0.1,  # 10% of errors events
+    traces_sample_rate=0.01,  # 1% of events of tracing
 )
