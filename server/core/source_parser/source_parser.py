9ace1a5db2b8df3812a8c9233d876f4c166d236e
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-09-04 10:58:40 +0400

    Update: run source tasks in one loop, same for fetcher tasks; make SourceParser async

--- server/core/source_parser/source_parser.py
+++ server/core/source_parser/source_parser.py
@@ -1,21 +1,22 @@
 from typing import Iterable, Optional
-from urllib.parse import urlparse
+from .parsers.base_parser import ParserBase
 
-from asgiref.sync import async_to_sync
-from django.db import transaction
+import re
 from lxml.html.clean import Cleaner
 from selectolax.parser import HTMLParser
+
+
+from .parsers.vk_parser import VkParser
+from .parsers.ok_parser import OkParser
+from .parsers.tg_parser import TgParser
+from .parsers.common_parser import CommonParser
+from .parsers.rss_parser import RssParser
 
 from server.apps.core.models import Article, Source
 from server.core.fetcher import Fetcher
 from server.libs.handler import HandlerRegistry
 
-from .parsers.base_parser import ParserBase
-from .parsers.common_parser import CommonParser
-from .parsers.ok_parser import OkParser
-from .parsers.rss_parser import RssParser
-from .parsers.tg_parser import TgParser
-from .parsers.vk_parser import VkParser
+
 
 CLEANER = Cleaner(
     scripts=True,
@@ -62,22 +63,27 @@ def build_document(html, clean=False):
     return document
 
 
-def add_articles(source: Source, urls: list[str]) -> list[Article]:
+from asgiref.sync import sync_to_async
+
+
+async def add_articles(
+    source: Source, articles: list[Union[str, Article]]
+) -> list[Article]:
+    pattern = re.compile(r"https?://(?P<url_without_method>.+)")
     added = []
 
     for url in urls:
-        with transaction.atomic():
-            if not Article.objects.filter(url__iendswith=url.split("://")[1]).exists():
-                try:
-                    article, created = Article.objects.get_or_create(
-                        url=url, source=source
-                    )
-                    if created:
-                        added.append(article)
-                except Exception as e:
-                    raise type(e)(
-                        f"When adding articles with {url} exception occurred: {e}"
-                    )
+        match = pattern.match(url)
+        if not match:
+            continue
+
+        url_without_method = match.group("url_without_method")
+
+        # unefficient:
+        if not await sync_to_async(
+            Article.objects.filter(url__iendswith=url_without_method).exists
+        )():
+            added.append(article)
 
     return added
 
@@ -102,9 +108,12 @@ class SourceParser:
         return parser.extract_urls(url, document)
 
     @classmethod
-    def create_new_articles(cls, source: Source) -> int:
-        urls = async_to_sync(cls.extract_all_news_urls)(source)
+    async def create_new_articles(cls, source: Source) -> int:
+        urls = await cls.extract_all_news_urls(source)
         if not urls:
             return 0
-        added = add_articles(source, urls)
+
+        added = await add_articles(source, urls)
+        for article in added:
+            await sync_to_async(article.save)()
         return len(added)
