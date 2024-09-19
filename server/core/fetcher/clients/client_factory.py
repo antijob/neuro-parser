44cd54b075d31c55938054071a0c328098abd11d
ubiq <explesy@gmail.com>
2024-09-19 10:10:53 +0200

    upd: made client factory async

--- server/core/fetcher/clients/client_factory.py
+++ server/core/fetcher/clients/client_factory.py
@@ -1,13 +1,23 @@
 from typing import Optional
-from server.apps.core.models import Source, Article
+
+from server.apps.core.models import Article, Source
+from server.core.fetcher.libs.proxy import ProxyManager
 
 from .base_client import ClientBase
 from .http_client import HttpClient
 
 
 class ClientFactory:
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
     @staticmethod
-    def get_client(
+    async def get_client(
         source: Optional[Source] = None, article: Optional[Article] = None
     ) -> ClientBase:
+        if source.needs_proxy:
+            return HttpClient(proxy=ProxyManager.get_proxy())
         return HttpClient()
