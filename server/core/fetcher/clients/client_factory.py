6916a88509ca316681e3a0ff4a1eef5d7bd0d360
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-11-07 19:49:56 +0400

    fix: client imports

--- server/core/fetcher/clients/client_factory.py
+++ server/core/fetcher/clients/client_factory.py
@@ -3,6 +3,7 @@ from server.apps.core.models import Source, Article
 
 from .base_client import ClientBase
 from .http_client import HttpClient
+from .telethon_client import TelethonClient
 
 
 class ClientFactory:
